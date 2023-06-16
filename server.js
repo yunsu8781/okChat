76d29990afb0a4584cc940947a54fbbd0d5de1df
yunsulee <lws8905@gmail.com>
2023-06-16 10:32:46 +0900

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chat.js', function (req, res) {
+  res.sendFile(__dirname + '/chat.js');
+});
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
